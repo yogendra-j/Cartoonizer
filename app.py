43a4b03fb09f5f15675b3c73658748e325a0eefc
yj <yogendra.jaiswal@btech2013.iitgn.ac.in>
2020-09-01 12:24:06 +0530

    minor bug

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ import os
 from flask import Flask, redirect, url_for, request, render_template
 from catoonizer import *
 
+
 def createapp():
-   return app = Flask(__name__)
+    return Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
@@ -17,15 +18,19 @@ def upload():
         f = request.files['file']
 
         basepath = os.path.dirname(__file__)
-        file_path = os.path.join(
-            basepath, 'static', 'uploads', "xx." + f.filename.split('.')[-1])
+        file_path = os.path.join(basepath, 'static', 'uploads',
+                                 "xx." + f.filename.split('.')[-1])
 
         f.save(file_path)
         out = cv2.imread(file_path)
         out = makecartoon(out)
-        cv2.imwrite(os.path.join(
-            basepath, 'static', 'uploads', "xxx." + f.filename.split('.')[-1]), out)
-        return redirect(url_for('static', filename='uploads/' + "xxx." + f.filename.split('.')[-1]), code=301)
+        cv2.imwrite(
+            os.path.join(basepath, 'static', 'uploads',
+                         "xxx." + f.filename.split('.')[-1]), out)
+        return redirect(url_for('static',
+                                filename='uploads/' + "xxx." +
+                                f.filename.split('.')[-1]),
+                        code=301)
 
 
 if __name__ == '__main__':
